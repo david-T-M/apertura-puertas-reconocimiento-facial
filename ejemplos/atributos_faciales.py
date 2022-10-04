1c249d64c5cd20936854c4495743e513f1607994
David <david.torres@uaem.mx>
2022-10-04 12:04:08 -0500

    MQTT

--- ejemplos/atributos_faciales.py
+++ ejemplos/atributos_faciales.py
@@ -1,5 +1,5 @@
 from deepface import DeepFace
 ruta="./../faces/david.jpeg"
-obj=DeepFace.analyze(img_path="C:/Users/David/Documents/GitHub/apertura-puertas-reconocimiento-facial/faces/david.jpeg",actions=['age','gender','race', 'emotion'])
+obj=DeepFace.analyze(img_path="./faces/david.jpeg",actions=['age','gender','race', 'emotion'])
 print("El resultado del análisis es: ")
 print(obj)
--- ejemplos/face-recognition.py
+++ ejemplos/face-recognition.py
@@ -6,7 +6,7 @@ import pandas as pd
 #Buscar rostro
 print("Buscando rostro")
 
-#df = DeepFace.find(img_path = "img1.jp",db_path="/home/david/Documentos/github/apertura-puertas-reconocimiento-facial/bd")
+#df = DeepFace.find(img_path = "david.jpeg",db_path="./bd")
 df = DeepFace.find (img_path = "/home/david/Documentos/github/apertura-puertas-reconocimiento-facial/faces/zooey.jpg", db_path = "/home/david/Documentos/github/apertura-puertas-reconocimiento-facial/bd", enforce_detection = "false")
 print("Resultado")
 print(df)
